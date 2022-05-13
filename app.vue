e60d882e0cfb3c8e537f49aa0bbbee8ec48d1c73
kodeandre <andre.kodehode@gmail.com>
2022-05-13 22:06:40 +0200

    project file changes

--- app.vue
+++ app.vue
@@ -1,11 +1,56 @@
 <script setup lang="ts">
-  const route = useRoute()
-  useHead({
-    meta: [{name: 'og:title', content: `${route.meta.title}`}]
-  })
+
 </script>
 
 <template>
-  <Header />
-  <NuxtPage />
+  <div class="app">
+    <Head>
+      <Meta name="description" content="title" />
+      <Meta name="keywords" content="Typescript, Vue, CV, resume, portfolio" />
+      <Meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <Link rel="icon" type="image/svg" href="/logo.svg" />
+    </Head>
+
+    <Header class="Header" />
+    <NuxtPage class="Body" />
+    <Footer class="Footer" />
+  </div>
 </template>
+
+<style lang="scss">
+:root {
+  background-color: #222;
+  color: #f5f5f5;
+  font-family: arial;
+}
+html, body {
+  min-height: 100%;
+}
+a {
+  text-decoration: none;
+  color: #f5f5f5;
+}
+.app {
+  display: grid;
+  grid-auto-columns: 1fr;
+  grid-template-columns: 1fr;
+  grid-template-rows: auto 1fr auto;
+  gap: 0.5rem 0rem;
+  grid-template-areas:
+    "Header"
+    "Body"
+    "Footer";
+}
+::-webkit-scrollbar {
+width: 6px;
+}
+::-webkit-scrollbar-track {
+background-color: #12121200;
+}
+::-webkit-scrollbar-thumb {
+background-color: #fe9000;
+}
+::-webkit-scrollbar-thumb:hover {
+background: #fe9000;
+}
+</style>

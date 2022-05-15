ec431d4d7e2578630f6ddcc5b0335c1ebd22bb56
kodeandre <andre.kodehode@gmail.com>
2022-05-15 15:48:58 +0200

    Changed homepage

--- pages/index.vue
+++ pages/index.vue
@@ -8,12 +8,27 @@
       <Title>Home</Title>
     </Head>
 
-    <h2>Home page</h2>
+    <div>
+      <h2>Who am i</h2>
+      <p>My name is Andrè, i'm 24 years old</p>
+      <h2>About me</h2>
+      <p>I love learning new things in coding and tech 24/7 and do so in my free time with frameworks and languages so i have as much experience and knowledge as possible, since i like coding that much.</p>
+      <p>My hobbies include gaming, coding, Dungeons & Dragons, and i also really like traveling to other countries when i can, mostly the UK.</p>
+      <h2>My experience</h2>
+      <p>I've worked as a Hardware repairer/maintainer and a System Admin, but most notably i have been and am working as a Software Engineer. (All the details are on my LinkedIn in the footer down below)</p>
+      <p>On top of that i have many years of experience of working in teams but also by myself, often ending up in a leading/important role.</p>
+      <h2>My education</h2>
+      <p>I have a formal education in Media, with a focus on Photography, from Nordahl Grieg Videregående.</p>
+      <p>Most of my coding knowledge comes from a history of interest in coding and tech in general, but mostly from a coding course i took over the span of a year in Bergen called Kodehode.</p>
+    </div>
   </div>
 </template>
 
 <style scoped lang="scss">
 .body {
   margin-left: 1rem;
+}
+h2 {
+  color: #ffa500;
 }
 </style>
